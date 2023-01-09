78471322bddd95bb07854c54339fb1d6f54d3f45
Danilo Lemes <daniloleemes@hotmail.com>
2023-01-09 20:00:17 +0000

    Create a burst connection orchestrator so we can just connect directly using the first of char/map and character

---
+++ UnityClient/Assets/Scripts/Heimdallr/Core/Network/BurstConnectionOrchestrator.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using UnityEngine;
+
+namespace Heimdallr.Core.Network {
+    public class BurstConnectionOrchestrator : MonoBehaviour {
+
+        private NetworkClient NetworkClient;
+
+        private HC.NOTIFY_ZONESVR2 CurrentMapInfo;
+
+        private int CharServerIndex = 0;
+        private int CharIndex = 0;
+        private string Username = "danilo";
+        private string Password = "123456";
+        private string Host;
+
+        private void Start() {
+            NetworkClient = FindObjectOfType<NetworkClient>();
+        }
+
+        public void Init(int charServerIndex, int charIndex, string username, string password, string host) {
+            CharServerIndex = charServerIndex;
+            CharIndex = charIndex;
+            Username = username;
+            Password = password;
+            Host = host;
+
+            NetworkClient.HookPacket(AC.ACCEPT_LOGIN3.HEADER, OnLoginResponse);
+            NetworkClient.HookPacket(HC.ACCEPT_ENTER.HEADER, OnEnterResponse);
+            NetworkClient.HookPacket(HC.NOTIFY_ZONESVR2.HEADER, OnCharacterSelectionAccepted);
+            NetworkClient.HookPacket(HC.ACCEPT_MAKECHAR.HEADER, OnMakeCharAccepted);
+            NetworkClient.HookPacket(ZC.ACCEPT_ENTER2.HEADER, OnMapServerLoginAccepted);
+
+            Connect();
+        }
+
+        private void Connect() {
+            TryConnectAndLogin(Host, Username, Password);
+        }
+
+        private async void TryConnectAndLogin(string host, string username, string password) {
+            await NetworkClient.ChangeServer(host, 6900);
+            new CA.LOGIN(username, password, 10, 10).Send();
+        }
+
+        private async void ConnectToCharServer(AC.ACCEPT_LOGIN3 loginInfo, string charIp, CharServerInfo charServerInfo) {
+            await NetworkClient.ChangeServer(charIp, charServerInfo.Port);
+            NetworkClient.SkipBytes(4);
+
+            new CH.ENTER(loginInfo.AccountID, loginInfo.LoginID1, loginInfo.LoginID2, loginInfo.Sex).Send();
+        }
+
+        #region Packet Hooks
+        private void OnLoginResponse(ushort cmd, int size, InPacket packet) {
+            if(packet is AC.ACCEPT_LOGIN3 pkt) {
+                NetworkClient.State.LoginInfo = pkt;
+                NetworkClient.State.CharServer = pkt.Servers[CharServerIndex];
+                /**
+                 * If using docker for rA, this should point to same host as the login server
+                 * Docker sends the ips internally as 172 whatever
+                 */
+                ConnectToCharServer(pkt, NetworkClient.State.CharServer.IP.ToString(), NetworkClient.State.CharServer);
+            }
+        }
+
+        private void OnEnterResponse(ushort cmd, int size, InPacket packet) {
+            if(packet is HC.ACCEPT_ENTER pkt) {
+                NetworkClient.State.CurrentCharactersInfo = pkt;
+                
+                // if no character available, create one
+                if (pkt.Chars.Count == 0) {
+                    new CH.MAKE_CHAR2() {
+                        Name = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8),
+                        CharNum = 0
+                    }.Send();
+                } else {
+                    new CH.SELECT_CHAR(CharIndex).Send();
+                }
+            }
+        }
+
+        private void OnMapServerLoginAccepted(ushort cmd, int size, InPacket packet) {
+            if(packet is ZC.ACCEPT_ENTER2) {
+                // Pausing because we need to change scenes and
+                // have everything ready on the next scene
+                NetworkClient.PausePacketHandling();
+
+                var pkt = packet as ZC.ACCEPT_ENTER2;
+                var mapLoginInfo = new MapLoginInfo() {
+                    mapname = CurrentMapInfo.Mapname.Split('.')[0],
+                    PosX = pkt.PosX,
+                    PosY = pkt.PosY,
+                    Dir = pkt.Dir
+                };
+                NetworkClient.State.MapLoginInfo = mapLoginInfo;
+                Session.CurrentSession.SetCurrentMap(mapLoginInfo.mapname);
+            }
+        }
+
+        /**
+         * The only situation we'll end up is when we don't have any character to begin with 
+         * So we create a new one and as we're orchestrating, just connect with it.
+         */
+        private void OnMakeCharAccepted(ushort cmd, int size, InPacket packet) {
+            if(packet is HC.ACCEPT_MAKECHAR ACCEPT_MAKECHAR) {
+                NetworkClient.State.SelectedCharacter = ACCEPT_MAKECHAR.characterData;
+
+                new CH.SELECT_CHAR(0).Send();
+            }
+        }
+
+        private async void OnCharacterSelectionAccepted(ushort cmd, int size, InPacket packet) {
+            if(packet is HC.NOTIFY_ZONESVR2 currentMapInfo) {
+                CurrentMapInfo = currentMapInfo;
+                NetworkClient.Disconnect();
+
+                await NetworkClient.ChangeServer(currentMapInfo.IP.ToString(), currentMapInfo.Port);
+                NetworkClient.CurrentConnection.Start();
+
+                // TODO implement this part
+                //var entity = EntityManager.SpawnPlayer(NetworkClient.State.SelectedCharacter);
+                //Session.StartSession(new Session(entity, NetworkClient.State.LoginInfo.AccountID));
+                //DontDestroyOnLoad(entity.gameObject);
+
+                var loginInfo = NetworkClient.State.LoginInfo;
+                new CZ.ENTER2(loginInfo.AccountID, NetworkClient.State.SelectedCharacter.GID, loginInfo.LoginID1, new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds(), loginInfo.Sex).Send();
+            }
+        }
+        #endregion
+    }
+}
--- UnityClient/Assets/Scripts/Heimdallr/Core/UtilsManager.cs
+++ UnityClient/Assets/Scripts/Heimdallr/Core/UtilsManager.cs
@@ -1,7 +1,9 @@
 using Heimdallr.Core.Game;
+using Heimdallr.Core.Network;
 using TMPro;
 using UnityEngine;
 
+[RequireComponent(typeof(NetworkClient), typeof(ThreadManager))]
 public class UtilsManager : MonoBehaviour {
 
     [SerializeField]
@@ -11,19 +13,38 @@ public class UtilsManager : MonoBehaviour {
     private TextMeshProUGUI FpsLabel;
     private float _deltaTime;
 
-    private void Start() {
-        PlayerEntity.Init(new GameEntityData {
-            HairStyle = 0,
-            Eye = 0,
-            IsMale = false,
-            HairColor = 3,
-            Job = 3,
-            ClothesColor = 0,
-            MoveSpeed = 135,
-            EntityType = EntityType.PC
-        });
+    [Header(":: Test Server Only")]
+    // Whether to orchestrate the login journey
+    [SerializeField]
+    private bool OrchestrateConnect = true;
+    [SerializeField]
+    private int CharServerIndex = 0;
+    [SerializeField]
+    private int CharIndex = 0;
+    [SerializeField]
+    private string Username = "danilo";
+    [SerializeField]
+    private string Password = "123456";
+    [SerializeField]
+    private string ServerHost = "127.0.0.1";
 
-        PlayerEntity.transform.SetPositionAndRotation(new Vector3(150,0,170), Quaternion.identity);
+    private void Start() {
+        if(OrchestrateConnect) {
+            gameObject.AddComponent<BurstConnectionOrchestrator>().Init(CharServerIndex, CharIndex, Username, Password, ServerHost);
+        } else {
+            PlayerEntity.Init(new GameEntityData {
+                HairStyle = 0,
+                Eye = 0,
+                IsMale = false,
+                HairColor = 3,
+                Job = 3,
+                ClothesColor = 0,
+                MoveSpeed = 135,
+                EntityType = EntityType.PC
+            });
+
+            PlayerEntity.transform.SetPositionAndRotation(new Vector3(150, 0, 170), Quaternion.identity);
+        }
     }
 
     private void Update() {
