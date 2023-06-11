2f576a7ecdaaa15c18776f659636762273fc90b1
Danilo Menezes <danilo.menezes@sky.uk>
2023-06-11 09:46:38 +0100

    Start using custom version of Telepathy

--- UnityClient/Assets/Scripts/Heimdallr/Core/Network/BurstConnectionOrchestrator.cs
+++ UnityClient/Assets/Scripts/Heimdallr/Core/Network/BurstConnectionOrchestrator.cs
@@ -27,6 +27,8 @@ namespace Core.Network {
 
             NetworkClient.HookPacket<AC.ACCEPT_LOGIN3>(AC.ACCEPT_LOGIN3.HEADER, OnLoginResponse);
             NetworkClient.HookPacket<HC.ACCEPT_ENTER>(HC.ACCEPT_ENTER.HEADER, OnEnterResponse);
+            NetworkClient.HookPacket<HC.ACCEPT_ENTER2>(HC.ACCEPT_ENTER2.HEADER, OnEnter2Response);
+            NetworkClient.HookPacket<HC.NOTIFY_CHARLIST>(HC.NOTIFY_CHARLIST.HEADER, OnCharListResponse);
             NetworkClient.HookPacket<HC.NOTIFY_ZONESVR2>(HC.NOTIFY_ZONESVR2.HEADER, OnCharacterSelectionAccepted);
             NetworkClient.HookPacket<HC.ACCEPT_MAKECHAR>(HC.ACCEPT_MAKECHAR.HEADER, OnMakeCharAccepted);
             NetworkClient.HookPacket<ZC.ACCEPT_ENTER2>(ZC.ACCEPT_ENTER2.HEADER, OnMapServerLoginAccepted);
@@ -56,43 +58,41 @@ namespace Core.Network {
             TryConnectAndLogin(Host, Username, Password);
         }
 
-        private async void TryConnectAndLogin(string host, string username, string password) {
-            Debug.Log("Logging in");
-            await NetworkClient.ChangeServer(host, 6900);
+        private void TryConnectAndLogin(string host, string username, string password) {
+            //Debug.Log("Logging in");
+            NetworkClient.Connect(host, 6900, NetworkClient.ServerType.Login);
             new CA.LOGIN(username, password, 10, 10).Send();
         }
 
-        private async void ConnectToCharServer(
+        private void ConnectToCharServer(
             AC.ACCEPT_LOGIN3 loginInfo,
-            string charIp,
             CharServerInfo charServerInfo
         ) {
-            Debug.Log("Connecting to char server");
-            await NetworkClient.ChangeServer(Host, charServerInfo.Port);
-            NetworkClient.SkipBytes(4);
-
+            //Debug.Log("Connecting to char server");
+            NetworkClient.Connect(Host, charServerInfo.Port, NetworkClient.ServerType.Char);
             new CH.ENTER(loginInfo.AccountID, loginInfo.LoginID1, loginInfo.LoginID2, loginInfo.Sex).Send();
         }
 
         private void SelectCharacter(int index) {
-            Debug.Log("Selecting character");
+            //Debug.Log("Selecting character");
             new CH.SELECT_CHAR(index).Send();
         }
 
         #region Packet Hooks
 
         private void OnLoginResponse(ushort cmd, int size, AC.ACCEPT_LOGIN3 packet) {
-            Debug.Log("Login response received");
+            //Debug.Log("Login response received");
             NetworkClient.State.LoginInfo = packet;
             NetworkClient.State.CharServer = packet.Servers[CharServerIndex];
+            NetworkClient.SetAccountId(packet.AccountID);
 
             // If using docker for rA, this should point to same host as the login server
             // Docker sends the ips internally as 172 whatever
-            ConnectToCharServer(packet, NetworkClient.State.CharServer.IP.ToString(), NetworkClient.State.CharServer);
+            ConnectToCharServer(packet, NetworkClient.State.CharServer);
         }
 
         private void OnEnterResponse(ushort cmd, int size, HC.ACCEPT_ENTER pkt) {
-            Debug.Log("Char server response received");
+            //Debug.Log("Char server response received");
             NetworkClient.State.CurrentCharactersInfo = pkt;
 
             // if no character available, create one
@@ -108,7 +108,7 @@ namespace Core.Network {
         }
 
         private async void OnMapServerLoginAccepted(ushort cmd, int size, ZC.ACCEPT_ENTER2 pkt) {
-            Debug.Log("Map server response received");
+            //Debug.Log("Map server response received");
             NetworkClient.PausePacketHandling();
 
             var mapLoginInfo = new MapLoginInfo {
@@ -160,21 +160,29 @@ namespace Core.Network {
          * So we create a new one and as we're orchestrating, just connect with it.
          */
         private void OnMakeCharAccepted(ushort cmd, int size, HC.ACCEPT_MAKECHAR ACCEPT_MAKECHAR) {
-            Debug.Log("Char created");
+            //Debug.Log("Char created");
             NetworkClient.State.SelectedCharacter = ACCEPT_MAKECHAR.characterData;
 
             SelectCharacter(0);
         }
 
         private async void OnCharacterSelectionAccepted(ushort cmd, int size, HC.NOTIFY_ZONESVR2 currentMapInfo) {
-            Debug.Log("Char selection accepted");
+            //Debug.Log("Char selection accepted");
             CurrentMapInfo = currentMapInfo;
 
-            await NetworkClient.ChangeServer(Host, currentMapInfo.Port);
+            NetworkClient.Connect(Host, currentMapInfo.Port, NetworkClient.ServerType.Zone);
 
             var loginInfo = NetworkClient.State.LoginInfo;
             new CZ.ENTER2(loginInfo.AccountID, NetworkClient.State.SelectedCharacter.GID, loginInfo.LoginID1,
                 new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds(), loginInfo.Sex).Send();
+        }
+        
+        private void OnCharListResponse(ushort cmd, int size, HC.NOTIFY_CHARLIST packet) {
+            //Debug.Log("Char list received");
+        }
+        
+        private void OnEnter2Response(ushort cmd, int size, HC.ACCEPT_ENTER2 packet) {
+            //Debug.Log($"Accept enter 2");
         }
         #endregion
 
