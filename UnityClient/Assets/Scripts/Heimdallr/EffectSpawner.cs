ba5f1246400f63c6bd5ca8605bad35c68578c893
Danilo Lemes <daniloleemes@hotmail.com>
2024-10-04 21:26:15 +0100

    add fps counter to test build

--- UnityClient/Assets/Scripts/Heimdallr/EffectSpawner.cs
+++ UnityClient/Assets/Scripts/Heimdallr/EffectSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using _3rdparty.unityro_sdk.Core.Effects;
 using Core.Effects;
 using Core.Effects.EffectParts;
@@ -30,6 +31,23 @@ namespace Heimdallr
             {
                 renderer.Initialize(renderInfo);
             }
+        }
+        
+        private float count;
+    
+        private IEnumerator Start()
+        {
+            GUI.depth = 2;
+            while (true)
+            {
+                count = 1f / Time.unscaledDeltaTime;
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    
+        private void OnGUI()
+        {
+            GUI.Label(new Rect(5, 40, 100, 25), "FPS: " + Mathf.Round(count));
         }
     }
 }
