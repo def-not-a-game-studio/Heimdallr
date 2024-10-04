7814ab2a2098e5b20140b1b8b68d84a3b79efae1
Danilo Lemes <daniloleemes@hotmail.com>
2024-10-04 11:46:27 +0100

    create a few more effect files

--- UnityClient/Assets/Scripts/Heimdallr/EffectSpawner.cs
+++ UnityClient/Assets/Scripts/Heimdallr/EffectSpawner.cs
@@ -1,22 +1,21 @@
-using System;
-using System.Collections.Generic;
 using _3rdparty.unityro_sdk.Core.Effects;
 using Core.Effects;
-using ROIO.Models.FileTypes;
-using UnityEngine;
+using Core.Effects.EffectParts;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace Heimdallr
 {
     public class EffectSpawner : MonoBehaviour
     {
-        [SerializeField] private StrEffectRenderer renderer;
 
         private EffectCache _effectCache;
-
+        private StrEffectRenderer[] _strEffectRenderers;
+        
         private void Awake()
         {
             _effectCache = FindAnyObjectByType<EffectCache>();
+            _strEffectRenderers = FindObjectsByType<StrEffectRenderer>(FindObjectsInactive.Exclude, FindObjectsSortMode.InstanceID);
         }
 
         public void SpawnEffect()
@@ -26,15 +25,10 @@ namespace Heimdallr
 
         private async UniTaskVoid SpawnEffectAsync()
         {
-            var effect = await Resources.LoadAsync<STR>("Effects/STR/magnificat") as STR;
-            var renderInfo = _effectCache.GetRenderInfo(0);
-            if (renderInfo != null)
+            var renderInfo = await _effectCache.GetRenderInfo((int)EffectId.EF_BLESSING);
+            foreach (var renderer in _strEffectRenderers)
             {
-                renderer.Initialize(effect, 0, renderInfo);
-            }
-            else
-            {
-                renderer.Initialize(effect, 0);
+                renderer.Initialize(renderInfo);
             }
         }
     }
