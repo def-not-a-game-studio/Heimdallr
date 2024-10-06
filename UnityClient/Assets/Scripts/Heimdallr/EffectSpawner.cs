0c41e085658022066f0bf7ce46f0262d67df3972
Danilo Lemes <daniloleemes@hotmail.com>
2024-10-06 15:17:56 +0100

    migrate cylinders to not mono

--- UnityClient/Assets/Scripts/Heimdallr/EffectSpawner.cs
+++ UnityClient/Assets/Scripts/Heimdallr/EffectSpawner.cs
@@ -9,37 +9,50 @@ namespace Heimdallr
 {
     public class EffectSpawner : MonoBehaviour
     {
+        [SerializeField] private Effect effect;
 
         private EffectCache _effectCache;
         private EffectRenderer[] _strEffectRenderers;
-        
+
         private void Awake()
         {
             _effectCache = FindAnyObjectByType<EffectCache>();
-            _strEffectRenderers = FindObjectsByType<EffectRenderer>(FindObjectsInactive.Exclude, FindObjectsSortMode.InstanceID);
+            _strEffectRenderers =
+                FindObjectsByType<EffectRenderer>(FindObjectsInactive.Exclude, FindObjectsSortMode.InstanceID);
         }
 
         public void SpawnEffect()
         {
             SpawnEffectAsync();
         }
-        
+
         private void SpawnEffectAsync()
         {
             foreach (var renderer in _strEffectRenderers)
             {
-                renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_CONCENTRATION]);
-                renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_JOBLVUP]);
-                renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_MAGNIFICAT]);
-                renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_ANGELUS]);
-                renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_ANGEL]);
+                if (effect == null)
+                {
+                    renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_CONCENTRATION]);
+                    renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_JOBLVUP]);
+                    renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_MAGNIFICAT]);
+                    renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_ANGELUS]);
+                    renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_ANGEL]);
+                }
+                else
+                {
+                    renderer.SetEffect(effect);
+                }
             }
         }
-        
+
         private float count;
-    
+
         private IEnumerator Start()
         {
+            if (effect != null)
+            {
+                SpawnEffect();
+            }
             GUI.depth = 2;
             while (true)
             {
@@ -47,7 +60,7 @@ namespace Heimdallr
                 yield return new WaitForSeconds(0.1f);
             }
         }
-    
+
         private void OnGUI()
         {
             GUI.Label(new Rect(5, 40, 100, 25), "FPS: " + Mathf.Round(count));
