8109b64975c3a53109a54051e54a1eceabc71326
Danilo Lemes <daniloleemes@hotmail.com>
2025-02-15 21:28:27 +0000

    Create Audio manager system

--- UnityClient/Assets/Scripts/Heimdallr/EffectSpawner.cs
+++ UnityClient/Assets/Scripts/Heimdallr/EffectSpawner.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using _3rdparty.unityro_sdk.Core.Effects;
 using Core.Effects;
 using Core.Effects.EffectParts;
@@ -12,13 +14,13 @@ namespace Heimdallr
         [SerializeField] private Effect effect;
 
         private EffectCache _effectCache;
-        private EffectRenderer[] _strEffectRenderers;
+        private List<EffectRenderer> _strEffectRenderers;
 
         private void Awake()
         {
             _effectCache = FindAnyObjectByType<EffectCache>();
             _strEffectRenderers =
-                FindObjectsByType<EffectRenderer>(FindObjectsInactive.Exclude, FindObjectsSortMode.InstanceID);
+                FindObjectsByType<EffectRenderer>(FindObjectsInactive.Exclude, FindObjectsSortMode.InstanceID).ToList();
         }
 
         public void SpawnEffect()
@@ -26,22 +28,53 @@ namespace Heimdallr
             SpawnEffectAsync();
         }
 
-        private void SpawnEffectAsync()
+        private async void SpawnEffectAsync()
         {
+            var effs = new Effect[]
+            {
+                _effectCache.Effects[(int)EffectId.EF_CONCENTRATION],
+                _effectCache.Effects[(int)EffectId.EF_JOBLVUP],
+                _effectCache.Effects[(int)EffectId.EF_MAGNIFICAT],
+                _effectCache.Effects[(int)EffectId.EF_ANGELUS],
+                _effectCache.Effects[(int)EffectId.EF_ANGEL],
+            };
+            var effIndex = 0;
+            var count = 0;
+
+            while (_strEffectRenderers.Count > 0)
+            {
+                var randomIndex = Random.Range(0, _strEffectRenderers.Count);
+                var effectIndex = Random.Range(0, effs.Length);
+                var renderer = _strEffectRenderers[randomIndex];
+                var effect = effs[effectIndex];
+                renderer.SetEffect(effect);
+                await UniTask.Delay(Random.Range(0, 200));
+                _strEffectRenderers.Remove(renderer);
+            }
+
+            return;
             foreach (var renderer in _strEffectRenderers)
             {
                 if (effect == null)
                 {
-                    renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_CONCENTRATION]);
-                    renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_JOBLVUP]);
-                    renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_MAGNIFICAT]);
-                    renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_ANGELUS]);
-                    renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_ANGEL]);
+                    renderer.SetEffect(effs[effIndex]);
+                    // renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_CONCENTRATION]);
+                    // renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_JOBLVUP]);
+                    // renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_MAGNIFICAT]);
+                    // renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_ANGELUS]);
+                    // renderer.SetEffect(_effectCache.Effects[(int)EffectId.EF_ANGEL]);
                 }
                 else
                 {
                     renderer.SetEffect(effect);
                 }
+                
+                count++;
+                if (count % 15 == 0)
+                {
+                    effIndex = (effIndex + 1) % effs.Length;
+                }
+                await UniTask.Delay(Random.Range(0, 200));
             }
         }
 
@@ -53,6 +86,7 @@ namespace Heimdallr
             {
                 SpawnEffect();
             }
+
             GUI.depth = 2;
             while (true)
             {
