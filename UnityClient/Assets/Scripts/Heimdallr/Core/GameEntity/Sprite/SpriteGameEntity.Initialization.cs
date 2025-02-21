e6a36465a393ea878a2edb75a3eaed77416cf77c
Danilo Lemes <daniloleemes@hotmail.com>
2025-02-21 21:56:00 +0000

    FadeIn on spawn and FadeOut on Vanish

--- UnityClient/Assets/Scripts/Heimdallr/Core/GameEntity/Sprite/SpriteGameEntity.Initialization.cs
+++ UnityClient/Assets/Scripts/Heimdallr/Core/GameEntity/Sprite/SpriteGameEntity.Initialization.cs
@@ -13,7 +13,8 @@ namespace Heimdallr.Core.Game.Sprite
 {
     public partial class SpriteGameEntity
     {
-        private void Awake() {
+        private void Awake()
+        {
             SessionManager = FindObjectOfType<SessionManager>();
             PathFinder = FindObjectOfType<PathFinder>();
             EntityManager = FindObjectOfType<EntityManager>();
@@ -26,19 +27,21 @@ namespace Heimdallr.Core.Game.Sprite
         {
             SessionManager.OnSessionMapChanged -= this.OnMapChanged;
         }
-        
+
         private void OnMapChanged(GameMap map)
         {
             this._currentMap = map;
         }
 
-        private void Start() {
+        private void Start()
+        {
             MovementController = gameObject.GetOrAddComponent<GameEntityMovementController>();
             MovementController.SetEntity(this);
             _state = EntityState.Idle;
         }
 
-        private void HandleSpawnData() {
+        private void HandleSpawnData()
+        {
             if (_spawnPosDir == null) return;
 
             var x = _spawnPosDir.posDir[0];
@@ -46,22 +49,24 @@ namespace Heimdallr.Core.Game.Sprite
 
             var pos = new Vector3(x, PathFinder.GetCellHeight(x, y), y);
             transform.position = pos;
-            
+
             EffectRenderer = new GameObject("Renderer").AddComponent<EffectRenderer>();
             EffectRenderer.gameObject.layer = LayerMask.NameToLayer("Portal");
             EffectRenderer.transform.SetParent(gameObject.transform, false);
             EffectRenderer.transform.localPosition = new Vector3(0.5f, 0, 0.5f);
 
-            if ((JobType)_status.Job == JobType.JT_WARPNPC) {
+            if ((JobType)_status.Job == JobType.JT_WARPNPC)
+            {
                 SpriteViewer.gameObject.SetActive(false);
                 var resourceRequest = Resources.LoadAsync<Effect>("Database/Effects/WarpZone2");
-                resourceRequest.completed += (op) => {
-                    EffectRenderer.InitEffects(resourceRequest.asset as Effect);
-                };
+                resourceRequest.completed += (op) => { EffectRenderer.InitEffects(resourceRequest.asset as Effect); };
                 _spawnPosDir = null;
                 return;
-            } else {
-                switch (_status.EntityType) {
+            }
+            else
+            {
+                switch (_status.EntityType)
+                {
                     case EntityType.MOB:
                         SpriteViewer.gameObject.layer = LayerMask.NameToLayer("Monster");
                         break;
@@ -79,13 +84,15 @@ namespace Heimdallr.Core.Game.Sprite
                 }
             }
 
-            if (MovementController is null) {
+            if (MovementController is null)
+            {
                 MovementController = gameObject.GetOrAddComponent<GameEntityMovementController>();
             }
 
             MovementController.SetEntity(this);
 
-            if (_spawnPosDir.posDir.Length == 3) {
+            if (_spawnPosDir.posDir.Length == 3)
+            {
                 // standing/idle entry
                 var npcDirection = (NpcDirection)_spawnPosDir.posDir[2];
                 Direction = _spawnPosDir.forceNorthDirection ? Direction.North : npcDirection.ToDirection();
@@ -95,7 +102,8 @@ namespace Heimdallr.Core.Game.Sprite
             var bodySprite = (_status.EntityType != EntityType.PC || _status.IsMale) ? body.Male : body.Female;
             SpriteViewer.Init(bodySprite, ViewerType.Body, this);
 
-            if (_status.EntityType == EntityType.PC) {
+            if (_status.EntityType == EntityType.PC)
+            {
                 var head = DatabaseManager.GetHeadById(_status.HairStyle);
                 var headSprite = _status.IsMale ? head.Male : head.Female;
 
@@ -104,16 +112,18 @@ namespace Heimdallr.Core.Game.Sprite
                 headViewer.Init(headSprite, ViewerType.Head, this);
                 headViewer.gameObject.layer = LayerMask.NameToLayer("Player");
             }
-            
+
             // handle moving entry after initializing sprite viewer
-            if (_spawnPosDir.posDir.Length == 5) {
+            if (_spawnPosDir.posDir.Length == 5)
+            {
                 ProcessMovingEntry(_spawnPosDir.posDir);
             }
 
             _spawnPosDir = null;
         }
 
-        private void ProcessMovingEntry(int[] posDir) {
+        private void ProcessMovingEntry(int[] posDir)
+        {
             var x = posDir[0];
             var y = posDir[1];
             var x1 = posDir[2];
@@ -124,7 +134,8 @@ namespace Heimdallr.Core.Game.Sprite
             StartMoving((int)move.x, (int)move.y, (int)move.z, (int)move.w);
         }
 
-        public override void Init(GameEntityBaseStatus gameEntityBaseStatus) {
+        public override void Init(GameEntityBaseStatus gameEntityBaseStatus)
+        {
             DatabaseManager = FindObjectOfType<CustomDatabaseManager>();
 
             _status = gameEntityBaseStatus;
@@ -142,21 +153,27 @@ namespace Heimdallr.Core.Game.Sprite
             gameObject.SetActive(true);
         }
 
-        public override void Spawn(GameEntityBaseStatus status, int[] posDir, bool forceNorthDirection) {
+        public override void Spawn(GameEntityBaseStatus status, int[] posDir, bool forceNorthDirection)
+        {
             _status = status;
-            _spawnPosDir = new SpawnPosDir {
+            _spawnPosDir = new SpawnPosDir
+            {
                 posDir = posDir,
                 forceNorthDirection = forceNorthDirection
             };
             gameObject.SetActive(true);
             SpriteViewer.gameObject.SetActive(true);
+            StartCoroutine(SpriteViewer.FadeInRenderer(0, .5f));
         }
 
-        public override void UpdateStatus(GameEntityBaseStatus status, int[] posDir, bool forceNorthDirection) {
+        public override void UpdateStatus(GameEntityBaseStatus status, int[] posDir, bool forceNorthDirection)
+        {
             _status = status;
-            if (posDir.Length == 5) {
+            if (posDir.Length == 5)
+            {
                 ProcessMovingEntry(posDir);
             }
+
             gameObject.SetActive(true);
         }
     }
