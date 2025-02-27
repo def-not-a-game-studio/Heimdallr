685c7bcec06b1f77aaae6a29ac3dba5b556f61a8
Danilo Lemes <daniloleemes@hotmail.com>
2025-02-27 20:08:30 +0000

    hook some status values from packets

--- UnityClient/Assets/Scripts/Heimdallr/Core/GameEntity/Sprite/SpriteGameEntity.Status.cs
+++ UnityClient/Assets/Scripts/Heimdallr/Core/GameEntity/Sprite/SpriteGameEntity.Status.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityRO.Net;
 
@@ -35,7 +37,7 @@ namespace Heimdallr.Core.Game.Sprite
             _status.NeedInt = packet.needInte;
             _status.NeedDex = packet.needDex;
             _status.NeedLuk = packet.needLuk;
-            
+
             _status.Atk = packet.atk;
             _status.Atk2 = packet.atk2;
             _status.MatkMin = packet.matkMin;
@@ -50,13 +52,119 @@ namespace Heimdallr.Core.Game.Sprite
             _status.Crit = packet.crit;
             _status.Aspd = packet.aspd;
             _status.Aspd2 = packet.aspd2;
-            
+
             UpdateStatusUI();
         }
 
         private void HandleEntityStatus(EntityStatus status, long value)
         {
-         Debug.Log($"HandleEntityStatus {status}");   
+            switch (status)
+            {
+                case EntityStatus.SP_WEIGHT:
+                    _status.Weight = value;
+                    break;
+                case EntityStatus.SP_MAXWEIGHT:
+                    _status.MaxWeight = value;
+                    break;
+                case EntityStatus.SP_HIT:
+                    _status.Hit = (short)value;
+                    break;
+                case EntityStatus.SP_FLEE1:
+                    _status.Flee = (short)value;
+                    break;
+                case EntityStatus.SP_FLEE2:
+                    _status.Flee2 = (short)value;
+                    break;
+                case EntityStatus.SP_ASPD:
+                    _status.Aspd = (short)value;
+                    break;
+                case EntityStatus.SP_ATK1:
+                    _status.Atk = (short)value;
+                    break;
+                case EntityStatus.SP_ATK2:
+                    _status.Atk2 = (short)value;
+                    break;
+                case EntityStatus.SP_DEF1:
+                    _status.Def = (short)value;
+                    break;
+                case EntityStatus.SP_DEF2:
+                    _status.Def2 = (short)value;
+                    break;
+                case EntityStatus.SP_CRITICAL:
+                    _status.Crit = (short)value;
+                    break;
+                case EntityStatus.SP_MATK1:
+                    _status.MatkMin = (short)value;
+                    break;
+                case EntityStatus.SP_MATK2:
+                    _status.MatkMax = (short)value;
+                    break;
+                case EntityStatus.SP_MDEF1:
+                    _status.Mdef = (short)value;
+                    break;
+                case EntityStatus.SP_MDEF2:
+                    _status.Mdef2 = (short)value;
+                    break;
+                case EntityStatus.SP_HP:
+                    _status.HP = value;
+                    break;
+                case EntityStatus.SP_MAXHP:
+                    _status.MaxHP = value;
+                    break;
+                case EntityStatus.SP_SP:
+                    _status.SP = value;
+                    break;
+                case EntityStatus.SP_MAXSP:
+                    _status.MaxSP = value;
+                    break;
+                case EntityStatus.SP_BASEEXP:
+                    _status.BaseExp = value;
+                    break;
+                case EntityStatus.SP_NEXTBASEEXP:
+                    _status.NextBaseExp = value;
+                    break;
+                case EntityStatus.SP_JOBEXP:
+                    _status.JobExp = value;
+                    break;
+                case EntityStatus.SP_NEXTJOBEXP:
+                    _status.NextJobExp = value;
+                    break;
+                case EntityStatus.SP_SKILLPOINT:
+                    _status.SkillPoints = (short)value;
+                    break;
+                case EntityStatus.SP_PATK:
+                    _status.Patk = (short)value;
+                    break;
+                case EntityStatus.SP_SMATK:
+                    _status.Smatk = (short)value;
+                    break;
+                case EntityStatus.SP_RES:
+                    _status.Res = (short)value;
+                    break;
+                case EntityStatus.SP_MRES:
+                    _status.Mres = (short)value;
+                    break;
+                case EntityStatus.SP_HPLUS:
+                    _status.Hplus = (short)value;
+                    break;
+                case EntityStatus.SP_CRATE:
+                    _status.Crate = (short)value;
+                    break;
+                case EntityStatus.SP_TRAITPOINT:
+                    _status.TraitPoints = (short)value;
+                    break;
+                case EntityStatus.SP_AP:
+                    _status.Ap = (short)value;
+                    break;
+                case EntityStatus.SP_MAXAP:
+                    _status.MaxAp = (short)value;
+                    break;
+                default:
+                    Debug.Log($"HandleEntityStatus unknown status {status}");
+                    break;
+            }
+            
+            UpdateStatusUI();
         }
     }
 }
